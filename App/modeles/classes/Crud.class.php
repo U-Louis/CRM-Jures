884ef1c7f4ad3bf857a3395e6f7e31bd02896ea0
Louis <louis.urbani@yahoo.com>
2021-10-17 13:40:01 +0200

    class diag+

---
+++ App/modeles/classes/Crud.class.php
@@ -0,0 +1,39 @@
+<?php
+
+    Class Crud{
+
+        //=================== INIT =========================
+
+
+        //================= CONSTRUCTOR ====================
+        public function __construct(){
+        }
+
+        //================== SETTERS =======================        
+        //================== GETTERS =======================
+
+        //================== METHODS =======================
+        protected function read_all(){
+
+        }
+
+        protected function read_byParam(){
+
+        }
+
+        protected function create(){
+
+        }
+
+        protected function update(){
+
+        }
+
+        protected function delete(){
+
+        }
+
+
+        
+        //================== DISPLAY =======================
+    }

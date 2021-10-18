140b8358e51fbcaf44333430c04124d79b1ba969
Amandine Pimentel de Andrade <amandine.pda@gmail.com>
2021-10-18 16:09:15 +0200

    index, crudclass (readall, create, update, delete) modified, crudException added

--- App/index.php
+++ App/index.php
@@ -1,5 +1,6 @@
 <?php
-    require_once('../modeles/Connector.class.php');
+    require_once('modeles/classes/Connector.class.php');
+    require_once('modeles/classes/Crud.class.php');
 
     const RC = "<br />\n";
 
@@ -8,6 +9,19 @@
         $connexion = Connector::getConnexion();
 //var_dump($connexion);
         echo "CONNEXION REUSSIE" . RC;
+
+        // read_ByParam
+        // $test2=Crud::read_byParam("contact", "ID_Contact");
+
+        // create
+        // echo $test3=Crud::create("habilitation", array("ID_Habilitation"," Libelle_Habilitation"," DebutValidite_Habilitation"," FinValidite_Habilitation"," VisibleVALCES"," VisibleCERES"," EnAttenteValidation"),array("'h30'","'Web'","'1993-11-24'","'2003-11-24'",1,1,0));
+
+        // update
+        // echo $test4=Crud::update("habilitation",array("Libelle_Habilitation = 'Web'","DebutValidite_Habilitation='2009-10-30'"), "ID_Habilitation = 'h30'");
+
+        // delete
+        // echo $test5=Crud::delete("habilitation",array("ID_Habilitation='h30'"));
+        
     } catch (PDOException $e) {
         echo $e->getMessage() . RC;
         echo "ECHEC de CONNEXION à la BDD" . RC;
@@ -16,5 +30,14 @@
     }
 
     echo RC . "LE PROGRAMME CONTINUE ..." . RC;
+
+    // read_All
+    try{
+        $test= Crud::read_all("patate");
+        print_r($test.RC);
+
+    } catch(CrudException $e) {
+        echo $e->getMessage().RC;
+    }
     
 ?>
--- App/modeles/classes/Crud.class.php
+++ App/modeles/classes/Crud.class.php
@@ -1,5 +1,7 @@
 <?php
 
+    require_once('CrudException.class.php');
+    
     Class Crud{
 
         //=================== INIT =========================
@@ -13,46 +15,51 @@
         //================== GETTERS =======================
 
         //================== METHODS =======================
-        protected function read_all($table){
+        public static function read_all($table){
 
-            $sql = "SELECT * FROM".$table;
+            if($table=="contact"){
+                $sql = "SELECT * FROM ".$table;
 
-            // get connect
-            $connexionPDO = Connector::getConnexion();
+                // get connect
+                $connexionPDO = Connector::getConnexion();
 
-            // execute the request
-            $request = $connexionPDO->prepare($sql);
-vardump($request);
-            $request->execute(array($table));
+                // execute the request
+                $request = $connexionPDO->prepare($sql);
 
-            // define fetchmode
+                $request->execute();
 
+                // recover data
+                $records = $request->fetchAll(PDO::FETCH_ASSOC);
 
-            // recover data
-            $records = $request->fetchAll(FETCH_ASSOC);
+                // close cursor and connexion
+                $request->closeCursor();
+                Connector::disconnect();
 
-            // close cursor and connexion
-            $request->closeCursor();
-            Connector::disconnect();
+                echo $sql;
+                return $records;
+            }
+            else{
+                throw new CrudException('Erreur dans la lecture de ces entités');
+            }
 
-            return $records;
+            
         }
 
-        protected function read_byParam($table, $param){
+        public static function read_byParam($table, $param){
 
-            $sql = "SELECT ".$param."FROM ".$table;
+            $sql = "SELECT ".$param." FROM ".$table;
 
             //get connect
             $connexionPDO = Connector::getConnexion();
 
             // execute the request
             $request = $connexionPDO->prepare($sql);
-vardump($request);
-            $request->execute(array($table));
+var_dump($request);
+            $request->execute();
 
             //recover data
 
-            $record = $request->fetchAll(FETCH_ASSOC);
+            $records = $request->fetchAll(PDO::FETCH_ASSOC);
 
             //close cursor and connexion
             $request->closeCursor();
@@ -62,16 +69,113 @@ vardump($request);
 
         }
 
-        protected function create(){
+        public static function create($table,$tColumn,$tValue){
+            // get connect
+            $connexionPDO = Connector::getConnexion();
+
+            //array ttt
+            $columnString="";
+            $valueString="";
+
+            foreach ($tColumn as $column){
+                $columnString.=$column.",";
+            }
+
+            $columnString = substr($columnString, 0, -1);
+
+            print_r($columnString."<br>");
+
+            foreach ($tValue as $value){
+                $valueString.=$value.",";
+            }
+
+            $valueString = substr($valueString, 0, -1);
+
+            print_r($valueString."<br>");
+
+
+            //request
+            $sql="INSERT INTO ".$table." (".$columnString.") VALUES (".$valueString.")";
+
+            $request = $connexionPDO->prepare($sql);
+            $request->execute(array($table));
+
+            $count=$request->rowCount();
+
+            if ($count == 0) {
+                $message = "Lignes affectées : ".$count;
+            } else {
+                $message = "Confirmation : l'utilisateur a bien été ajouté.";
+            }
+            return $message;
+        }
+
+        public static function update($table,$tColumn,$condition){
+            // get connect
+            $connexionPDO = Connector::getConnexion();
+
+            //array ttt
+            $columnString="";
+
+            foreach ($tColumn as $column){
+                $columnString.=$column.",";
+            }
+
+            $columnString = substr($columnString, 0, -1);
+
+            print_r($columnString."<br>");
+
+            //request
+            $sql="UPDATE ".$table." SET ".$columnString." WHERE ".$condition;
+
+            $request = $connexionPDO->prepare($sql);
+            print_r($request);
+
+            $request->execute();
+
+
+            
+            $count=$request->rowCount();
+
+            if ($count == 0) {
+                $message = "Lignes affectées : ".$count;
+            } else {
+                $message = "Confirmation : l'utilisateur a bien été modifié.";
+            }
+            return $message;
 
         }
 
-        protected function update(){
+        public static function delete($table,$tCondition){
+            // get connect
+            $connexionPDO = Connector::getConnexion();
 
-        }
+            //array ttt
+            $conditionString="";
 
-        protected function delete(){
+            foreach ($tCondition as $condition){
+                $conditionString.=$condition.",";
+            }
 
+            $conditionString = substr($conditionString, 0, -1);
+
+            print_r($conditionString."<br>");
+            //request
+            $sql="DELETE FROM ".$table." WHERE ".$conditionString;
+
+            $request = $connexionPDO->prepare($sql);
+            print_r($request);
+
+            $request->execute();
+
+            $count=$request->rowCount();
+
+            if ($count == 0) {
+                $message = "Lignes affectées : ".$count;
+            } else {
+                $message = "Confirmation : l'utilisateur a bien été supprimé.";
+            }
+            return $message;
         }
 
 

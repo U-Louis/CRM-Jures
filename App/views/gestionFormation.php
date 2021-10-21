f4bbf0bc7491845ecd8219f6407d88dbf4019d7f
Louis <69362039+U-Louis@users.noreply.github.com>
2021-10-21 16:30:07 +0200

    contact objectified OK

--- App/views/gestionFormation.php
+++ App/views/gestionFormation.php
@@ -18,6 +18,12 @@
                 $tempInstance = new mgr_formation();
                 $list = $tempInstance->read_all('formation');
 
+${'contact'. '5'} = new Contact('5');
+var_dump(${'contact'. '5'} );
+
+${'entreprise' . 'x'} = new Entreprise('2', $contact5);
+var_dump(${'entreprise' . 'x'});
+
                 foreach($list as $item){
                     echo '<tr>' .
                     '<td>' . $item['Libelle_Formation'] . '</td>'.

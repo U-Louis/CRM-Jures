5b61fe7a514f025b9508174fd155b964563d9db4
Louis <69362039+U-Louis@users.noreply.github.com>
2021-10-21 12:39:35 +0200

    Read formation OK

--- App/modeles/classes/mgr_formation.class.php
+++ App/modeles/classes/mgr_formation.class.php
@@ -15,7 +15,16 @@
      */
     public static function read_all() {
         $sql = 
-            'SELECT * FROM formation';
+            'SELECT fn.`ID_formation`, fn.`Libelle_Formation`, fn.`Date_DebutFormation`, fn.`Date_FinFormation`, c.`nom_contact`, fp.`Libelle_formationPatern`	
+             FROM formation fn
+             JOIN formationPattern fp
+             ON fn.ID_formationPattern = fp.ID_formationPattern
+             JOIN formateur fr
+             ON fn.ID_formateur = fr.ID_formateur
+             JOIN contact c
+             ON fr.ID_contact = c.ID_contact
+            '
+            ;
         $connexion = Connector::connect();
         $res = $connexion->prepare($sql);
         $res->execute();
